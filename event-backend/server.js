1d07caa1bcc687a40f8f27effc9d94880328e348
Sparsh Bhardwaj <sparshbhardwaj209@gmail.com>
2025-02-11 14:23:30 +0530

    added events routes in the server.js

--- event-backend/server.js
+++ event-backend/server.js
@@ -1,18 +1,20 @@
-import express from 'express';
+import express from "express";
 import connectDB from "./config/db.js";
-import authRoutes from './routes/auth.js';
-import eventRoutes from './routes/events.js';
+import authRoutes from "./routes/auth.js";
+import eventRoutes from "./routes/events.js";
 import "dotenv/config";
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+
 connectDB();
 app.use(express.json());
 
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/events', eventRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/events", eventRoutes);
+
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
