40bbcad073ab36e8ad860092cea76b4e054c9a15
Sparsh Bhardwaj <sparshbhardwaj209@gmail.com>
2025-02-11 10:44:06 +0530

    added bcrypt and Login API

--- event-backend/routes/auth.js
+++ event-backend/routes/auth.js
@@ -1,5 +1,6 @@
 import express from "express";
 import User from "../models/User.js";
+import bcrypt from 'bcrypt';
 
 const router = express.Router();
 
@@ -10,6 +11,20 @@ router.post("/register", async (req, res) => {
     const user = new User({ email, password });
     await user.save();
     console.log("User created successfully");
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+// Login
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user || !bcrypt.compare(password, user.password)) {
+      return res.status(401).send("Invalid credentials");
+    }
+    res.send("User logged in successfully");
   } catch (err) {
     res.status(400).send(err.message);
   }
